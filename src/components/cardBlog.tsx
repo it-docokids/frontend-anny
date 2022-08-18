f15c1551d455a770b1989778391f1d15b31db392
anydayr <annyrondon491@gmail.com>
2022-08-18 17:00:19 -0400

    card

---
+++ src/components/cardBlog.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+export default function CardBlog() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+         Title
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          autor
+        </Typography>
+      </CardContent>
+      
+    </Card>
+  );
+}
