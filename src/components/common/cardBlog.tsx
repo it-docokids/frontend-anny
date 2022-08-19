862a5ce2a04062a37678fb6f6d4371d77abec345
anydayr <annyrondon491@gmail.com>
2022-08-19 17:09:37 -0400

    Style: ajuste de estilos en card

--- src/components/common/cardBlog.tsx
+++ src/components/common/cardBlog.tsx
@@ -6,37 +6,42 @@ import Typography from "@mui/material/Typography";
 import { Icon } from "@mui/material";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import { blogs } from "../../types/api";
-
+import { Link } from "react-router-dom";
 export default function CardBlog(props: blogs) {
   return (
     <Card
       sx={{
-        maxWidth: 345,
+        maxWidth: 420,
         borderRadius: "10px",
         fontFamily: `"Heebo",sansSerif'`,
       }}
     >
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={props.image}
-          alt={props.title}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {props.author.first_name} {props.author.last_name}-
-            {props.created_at}
-          </Typography>
-          <Icon sx={{ color: "#919294" }}>visibility_icon</Icon>
-          <Typography variant="body2" color="text.secondary">
-            {props.intro}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia
+        component="img"
+        height="140"
+        image={props.image}
+        alt={props.title}
+        sx={{ height: "auto !important" }}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          <b>{props.title}</b>
+        </Typography>
+        <Typography variant="subtitle2" color="text.primary">
+          <Link to="#">
+            <i>
+              {props.author.first_name} {props.author.last_name}-
+              {props.created_at}
+            </i>
+          </Link>
+        </Typography>
+
+        <Icon sx={{ color: "#919294" }}>visibility_outlined_icon</Icon>
+        <Typography variant="subtitle1" color="text.secondary">
+          {props.intro}
+        </Typography>
+      </CardContent>
+
       <CardActions>
         <Button
           size="small"
@@ -46,6 +51,7 @@ export default function CardBlog(props: blogs) {
             background: "#18b4b0",
             borderRadius: "10px",
             padding: ".375rem .75rem",
+            textTransform: "capitalize",
           }}
         >
           Leer más
--- src/components/layout/cardBlogLayout.tsx
+++ src/components/layout/cardBlogLayout.tsx
@@ -1,18 +1,89 @@
 import React from "react";
 import CardBlog from "../common/cardBlog";
+import Grid from "@mui/material/Grid";
+import { Box } from "@mui/material";
+import imagen from "../../assets/blog_pico.png";
+import Paper from "@mui/material/Paper";
 export default function CardBlogLayout() {
   return (
-    <div>
-      <CardBlog
-        id={0}
-        image={
-          "https://www.google.com/search?q=gripe&rlz=1C1ONGR_esVE989VE989&sxsrf=ALiCzsb993XPRgFsgkvbT6E5-iyQXhKa6g:1660930397433&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjAldfyt9P5AhVFbTABHYcVBMYQ_AUoAXoECAEQAw&biw=1309&bih=636&dpr=1.1#imgrc=0EI6Rhqj0ogjLM"
-        }
-        title={"gripa"}
-        intro={"intro gripe"}
-        author={{ first_name: "María", last_name: "Martinez" }}
-        created_at={"01/02"}
-      />
-    </div>
+    <Paper
+      sx={{
+        p: 2,
+        margin: "auto",
+        maxWidth: 960,
+        flexGrow: 1,
+      }}
+    >
+      {" "}
+      <Grid
+        container
+        flexDirection="row"
+        spacing={3}
+        alignItems="stretch"
+        justifyContent="center"
+        alignContent={"center"}
+      >
+        <Grid item xs={12} sm={6} md={6}>
+          <CardBlog
+            id={0}
+            image={imagen}
+            title={" ¡Este pico está terrible! "}
+            intro={
+              " Soy pediatra y cofundadora de Docokids, llevo escuchando sobre gripas muchos años y he vivido picos respiratorios en el invierno de Estados Unidos. Puedo decir ¡este pico respiratorio en Colombia, está terrible! "
+            }
+            author={{ first_name: "Natalia", last_name: "Cano" }}
+            created_at={"01/02"}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={6}>
+          <CardBlog
+            id={0}
+            image={imagen}
+            title={" ¡Este pico está terrible! "}
+            intro={
+              " Soy pediatra y cofundadora de Docokids, llevo escuchando sobre gripas muchos años y he vivido picos respiratorios en el invierno de Estados Unidos. Puedo decir ¡este pico respiratorio en Colombia, está terrible! "
+            }
+            author={{ first_name: "Natalia", last_name: "Cano" }}
+            created_at={"01/02"}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={6}>
+          <CardBlog
+            id={0}
+            image={imagen}
+            title={" ¡Este pico está terrible! "}
+            intro={
+              " Soy pediatra y cofundadora de Docokids, llevo escuchando sobre gripas muchos años y he vivido picos respiratorios en el invierno de Estados Unidos. Puedo decir ¡este pico respiratorio en Colombia, está terrible! "
+            }
+            author={{ first_name: "Natalia", last_name: "Cano" }}
+            created_at={"01/02"}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={6}>
+          <CardBlog
+            id={0}
+            image={imagen}
+            title={" ¡Este pico está terrible! "}
+            intro={
+              " Soy pediatra y cofundadora de Docokids, llevo escuchando sobre gripas muchos años y he vivido picos respiratorios en el invierno de Estados Unidos. Puedo decir ¡este pico respiratorio en Colombia, está terrible! "
+            }
+            author={{ first_name: "Natalia", last_name: "Cano" }}
+            created_at={"01/02"}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={6}>
+          <CardBlog
+            id={0}
+            image={imagen}
+            title={" ¡Este pico está terrible! "}
+            intro={
+              " Soy pediatra y cofundadora de Docokids, llevo escuchando sobre gripas muchos años y he vivido picos respiratorios en el invierno de Estados Unidos. Puedo decir ¡este pico respiratorio en Colombia, está terrible! "
+            }
+            author={{ first_name: "Natalia", last_name: "Cano" }}
+            created_at={"01/02"}
+          />
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
